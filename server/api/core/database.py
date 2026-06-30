67a5755d8154825df3bc8fa11c5715c6b85e6481
parthergk <gauravkumar81464@gmail.com>
2026-06-30 12:39:12 +0530

    fix the db connection to connect with remote db

--- server/api/core/database.py
+++ server/api/core/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 DATABASE_URL= os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL);
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLoacal = sessionmaker(
     autocommit=False,
