8fcc8ba0b16ee726b6a591a9675a5dc371142cf9
parthergk <gauravkumar81464@gmail.com>
2026-06-30 12:07:32 +0530

    restructure the file and folder name to deploy on vercel

--- server/api/index.py
+++ server/api/index.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from core.database import Base, engine
